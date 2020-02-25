c1cb55615711870a9731ab593170697fdc9f5c1c
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2020-02-25 12:00:00 +0330

    my first line of code

---
+++ first.py
@@ -0,0 +1,5 @@
+'''
+Sadly, I don't have the source code. Just there is a link to the post I made 
+on the day I programmed for the first time in my life.
+https://www.instagram.com/p/CABGJMWJSCm/?igshid=MDJmNzVkMjY=
+'''
